985a21f1eb76632eef9d03e4978b52340541c114
Prakash436 <102865908+Prakash436@users.noreply.github.com>
2024-06-02 13:28:41 +0530

    Fix 3

--- app.py
+++ app.py
@@ -18,9 +18,14 @@ import numpy as np
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-@st.cache_resource
-def decorder_model(_model_path):
-    model = load_model(_model_path)
+# Custom hash function that returns a constant value
+def ignore_hash(obj):
+    return 0
+
+# Function to load a model with custom hash function
+@st.cache_resource(hash_funcs={type(load_model('model.h5')): ignore_hash})
+def decorder_model():
+    model = load_model('model.h5')
     return model
 
 @st.cache_resource
@@ -40,7 +45,7 @@ st.write("""
         """)
 
 #loading models and tokenizer
-model = decorder_model('model.h5')
+model = decorder_model()
 resnet_model = load_resnet()
 tokenizer = load_tokenizer()
 
